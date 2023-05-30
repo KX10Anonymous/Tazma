d46c34f01120a2fbfb4de45bf104ea4c254d5d61
KX10Anonymous <kx10anonymous@gmail.com>
2023-05-30 15:09:16 +0200

    NEW

---
+++ src/main/java/com/janonimo/tazma/TazmaApplication.java
@@ -0,0 +1,13 @@
+package com.janonimo.tazma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TazmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TazmaApplication.class, args);
+	}
+
+}
