5e5ba5ccf69909c0757b09b5257da22eb470f52e
KX10Anonymous <kx10anonymous@gmail.com>
2023-05-30 15:44:43 +0200

    Netbeans Link Up Test Signed-off-by:KX10Anonymous <kx10anonymous@gmail.com>

---
+++ src/main/java/com/janonimo/tazma/NewController.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/SpringFramework/Controller.java to edit this template
+ */
+package com.janonimo.tazma;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ *
+ * @author JANONIMO
+ */
+@Controller
+public class NewController {
+    
+    @RequestMapping("/url")
+    public String page(Model model) {
+        model.addAttribute("attribute", "value");
+        return "view.name";
+    }
+    
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Error message")
+    public void handleError() {
+    }
+    
+}
